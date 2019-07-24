aa496bd9da4b6619af40bed076b13c680ef9ba87
FaisalAlsharari <mr.fasoool.11@hotmail.com>
2019-07-25 01:13:59 +0300

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Course.create(code: 435,name: 'SEI'
+)
